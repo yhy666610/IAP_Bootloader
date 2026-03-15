ecefcd4e4860fe1e25fb487c9636d333fd88b7fa
lanxi <1748440980@qq.com>
2026-03-16 00:34:47 +0800

    <ref>: 优化crc32接口

--- app/fw_crypto.c
+++ app/fw_crypto.c
@@ -9,6 +9,7 @@
 
 #include <string.h>
 #include "fw_crypto.h"
+#include "crc32.h"
 
 /* AES S-Box（正变换查找表，256字节） */
 static const uint8_t s_sbox[256] =
@@ -305,24 +306,5 @@ void fw_crypto_decrypt(const uint8_t *in, uint8_t *out, uint32_t len,
  */
 bool fw_crypto_verify_crc32(const uint8_t *data, uint32_t len, uint32_t expected_crc32)
 {
-    /* 与项目中已有的crc32算法相同（third_lib/crc/crc32.c），此处独立实现避免头文件依赖 */
-    static const uint32_t crc_table[16] =
-    {
-        0x00000000UL, 0x1DB71064UL, 0x3B6E20C8UL, 0x26D930ACUL,
-        0x76DC4190UL, 0x6B6B51F4UL, 0x4DB26158UL, 0x5005713CUL,
-        0xEDB88320UL, 0xF00F9344UL, 0xD6D6A3E8UL, 0xCB61B38CUL,
-        0x9B64C2B0UL, 0x86D3D2D4UL, 0xA00AE278UL, 0xBDBDF21CUL
-    };
-
-    uint32_t crc = 0xFFFFFFFFUL;
-    uint32_t i;
-
-    for (i = 0; i < len; i++)
-    {
-        crc = (crc >> 4) ^ crc_table[(crc ^ (data[i] >> 0)) & 0x0F];
-        crc = (crc >> 4) ^ crc_table[(crc ^ (data[i] >> 4)) & 0x0F];
-    }
-
-    crc ^= 0xFFFFFFFFUL;
-    return (crc == expected_crc32);
+    return (crc32(data, len) == expected_crc32);
 }
